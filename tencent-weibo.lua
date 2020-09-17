b5d57f2a54f1d33dd3baeb0fb05b80a99660a2ca
arkiver <arkiver@protonmail.com>
2020-09-17 15:54:26 +0200

    initial

---
+++ tencent-weibo.lua
@@ -0,0 +1,353 @@
+dofile("table_show.lua")
+dofile("urlcode.lua")
+JSON = (loadfile "JSON.lua")()
+local urlparse = require("socket.url")
+local http = require("socket.http")
+
+local item_value = os.getenv('item_value')
+local item_type = os.getenv('item_type')
+local item_dir = os.getenv('item_dir')
+local warc_file_base = os.getenv('warc_file_base')
+
+local url_count = 0
+local tries = 0
+local downloaded = {}
+local addedtolist = {}
+local abortgrab = false
+
+local requeue_count = {}
+local discovered = {}
+discovered[item_value] = true
+
+local post_ids = {}
+
+for ignore in io.open("ignore-list", "r"):lines() do
+  downloaded[ignore] = true
+end
+
+load_json_file = function(file)
+  if file then
+    return JSON:decode(file)
+  else
+    return nil
+  end
+end
+
+discover_user = function(user, tries)
+  if tries == nil then
+    tries = 0
+  end
+  if discovered[user] then
+    return true
+  end
+  io.stdout:write("Discovered user " .. user .. ".\n")
+  io.stdout:flush()
+  local body, code, headers, status = http.request(
+    "http://blackbird-amqp.meo.ws:23038/tencentweibo-dlrw6xmf4iwbu7qxoekm/",
+    "user:" .. user
+  )
+  if code == 200 or code == 409 then
+    discovered[user] = true
+    return true
+  elseif code == 404 then
+    io.stdout:write("Project key not found.\n")
+    io.stdout:flush()
+  elseif code == 400 then
+    io.stdout:write("Bad format.\n")
+    io.stdout:flush()
+  else
+    io.stdout:write("Could not queue discovered user. Retrying...\n")
+    io.stdout:flush()
+    if tries == 10 then
+      io.stdout:write("Maximum retries reached for sending discovered user.\n")
+      io.stdout:flush()
+      return false
+    end
+    os.execute("sleep " .. math.pow(2, tries))
+    return discover_user(user, tries + 1)
+  end
+  return false
+end
+
+read_file = function(file)
+  if file then
+    local f = assert(io.open(file))
+    local data = f:read("*all")
+    f:close()
+    return data
+  else
+    return ""
+  end
+end
+
+allowed = function(url, parenturl)
+  if string.match(urlparse.unescape(url), "[<>\\%*%$;%^%[%],%(%){}]")
+    or string.match(url, "[%?&]lang=[a-z][a-z]_[A-Z][A-Z]")
+    or string.match(url, "[%?&]pagesetHome")
+    or string.match(url, "^https?://t%.qq%.com/guest/")
+    or string.match(url, "^https?://t%.qq%.com/login%.php")
+    or string.match(url, "^https?://t%.qq%.com/old/message%.php%?id=[0-9]+$")
+    or string.match(url, "^https?://api%.t%.qq%.com/old/message%.php%?id=[0-9]+&format=1$") then
+    return false
+  end
+
+  local tested = {}
+  for s in string.gmatch(url, "([^/]+)") do
+    if tested[s] == nil then
+      tested[s] = 0
+    end
+    if tested[s] == 6 then
+      return false
+    end
+    tested[s] = tested[s] + 1
+  end
+
+  if string.match(url, "^https?://t%.qq%.com/p/t/[0-9]+$")
+    and parenturl
+    and not string.match(parenturl, "^https?://t%.qq%.com/p/t/[0-9]+") then
+    post_ids[string.match(url, "([0-9]+)$")] = true
+  end
+
+  if string.match(url, "^https?://[^/]*qlogo%.cn/")
+    or string.match(url, "^https?://[^/]*qpic%.cn/") then
+    return true
+  end
+
+  for s in string.gmatch(url, "([a-zA-Z0-9%-_]+)") do
+    if s == item_value then
+      return true
+    end
+  end
+
+  local match = string.match(url, "^https?://t%.qq%.com/([a-zA-Z0-9%-_]+)")
+  if match and not discover_user(match) then
+    io.stdout:write("Error queuing user.\n")
+    io.stdout:flush()
+    abortgrab = true
+  end
+
+  for s in string.gmatch(url, "([0-9]+)") do
+    if post_ids[s] then
+      return true
+    end
+  end
+
+  return false
+end
+
+wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_parsed, iri, verdict, reason)
+  local url = urlpos["url"]["url"]
+  local html = urlpos["link_expect_html"]
+
+  if (downloaded[url] ~= true and addedtolist[url] ~= true)
+    and (allowed(url, parent["url"]) or html == 0) then
+    addedtolist[url] = true
+    return true
+  end
+  
+  return false
+end
+
+wget.callbacks.get_urls = function(file, url, is_css, iri)
+  local urls = {}
+  local html = nil
+  
+  downloaded[url] = true
+
+  local function check(urla)
+    local origurl = url
+    local url = string.match(urla, "^([^#]+)")
+    local url_ = string.match(url, "^(.-)%.?$")
+    url_ = string.gsub(url_, "&amp;", "&")
+    url_ = string.match(url_, "^(.-)%s*$")
+    url_ = string.match(url_, "^(.-)%??$")
+    url_ = string.match(url_, "^(.-)&?$")
+    if string.match(url_, "^https://[^/]*t%.qq%.com/") then
+      url_ = string.gsub(url_, "^https://", "http://")
+    end
+    if (downloaded[url_] ~= true and addedtolist[url_] ~= true)
+      and allowed(url_, origurl) then
+      table.insert(urls, { url=url_ })
+      addedtolist[url_] = true
+      addedtolist[url] = true
+    end
+  end
+
+  local function checknewurl(newurl)
+    if string.match(newurl, "^https?:////") then
+      check(string.gsub(newurl, ":////", "://"))
+    elseif string.match(newurl, "^https?://") then
+      check(newurl)
+    elseif string.match(newurl, "^https?:\\/\\?/") then
+      check(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^\\/") then
+      checknewurl(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^//") then
+      check(urlparse.absolute(url, newurl))
+    elseif string.match(newurl, "^/") then
+      check(urlparse.absolute(url, newurl))
+    elseif string.match(newurl, "^%.%./") then
+      if string.match(url, "^https?://[^/]+/[^/]+/") then
+        check(urlparse.absolute(url, newurl))
+      else
+        checknewurl(string.match(newurl, "^%.%.(/.+)$"))
+      end
+    elseif string.match(newurl, "^%./") then
+      check(urlparse.absolute(url, newurl))
+    end
+  end
+
+  local function checknewshorturl(newurl)
+    if string.match(newurl, "^%?") then
+      check(urlparse.absolute(url, newurl))
+    elseif not (string.match(newurl, "^https?:\\?/\\?//?/?")
+        or string.match(newurl, "^[/\\]")
+        or string.match(newurl, "^%./")
+        or string.match(newurl, "^[jJ]ava[sS]cript:")
+        or string.match(newurl, "^[mM]ail[tT]o:")
+        or string.match(newurl, "^vine:")
+        or string.match(newurl, "^android%-app:")
+        or string.match(newurl, "^ios%-app:")
+        or string.match(newurl, "^%${")) then
+      check(urlparse.absolute(url, newurl))
+    end
+  end
+
+  if allowed(url, nil)
+    and not string.match(url, "^https?://[^/]*qlogo%.cn/")
+    and not string.match(url, "^https?://[^/]*qpic%.cn/") then
+    html = read_file(file)
+    if string.match(url, "^https?://api%.t%.qq%.com/")
+      and string.match(html, '^{"') then
+      local data = load_json_file(html)
+      if data["info"] then
+        html = data["info"]
+      end
+      local real_count = tonumber(string.match(html, '<li class="select">[^%(<]+%(?([^<%)]*)%)?</li>'))
+      if real_count == nil then
+        real_count = 0
+      end
+      local _, received_count = string.gsub(html, '<div class="msgBox">', "")
+      if real_count > received_count then
+        io.stdout:write("Received fewer replies than available.\n")
+        if requeue_count[url] == nil then
+          requeue_count[url] = 0
+        end
+        if requeue_count[url] < 10 then
+          requeue_count[url] = requeue_count[url] + 1
+          io.stdout:write("Requeuing URL.\n")
+          table.insert(urls, { url=url })
+        else
+          io.stdout:write("Max requeues reached.\n")
+        end
+        io.stdout:flush()
+      end
+    end
+    for match in string.gmatch(html, "{url%s*:%s*'http://api%.t%.qq%.com/asyn/tag_oper_n.php'%s*,%s*data:%s*{([^}]+)}") do
+      --TODO
+    end
+    for url, params in string.gmatch(html, "url%s*:%s*'(http://api%.t%.qq%.com/old/message.php[^']+)'%s*,%s*auto%s*:%s*'([^']+)'") do
+      check(url .. "&" .. params .. "&g_tk=")
+    end
+    for newurl in string.gmatch(string.gsub(html, "&quot;", '"'), '([^"]+)') do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(string.gsub(html, "&#039;", "'"), "([^']+)") do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, ">%s*([^<%s]+)") do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, "href='([^']+)'") do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, "[^%-]href='([^']+)'") do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, '[^%-]href="([^"]+)"') do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, ":%s*url%(([^%)]+)%)") do
+      checknewurl(newurl)
+    end
+  end
+
+  return urls
+end
+
+wget.callbacks.httploop_result = function(url, err, http_stat)
+  status_code = http_stat["statcode"]
+  
+  url_count = url_count + 1
+  io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. "  \n")
+  io.stdout:flush()
+
+  if status_code >= 300 and status_code <= 399 then
+    local newloc = urlparse.absolute(url["url"], http_stat["newloc"])
+    if downloaded[newloc] == true or addedtolist[newloc] == true
+      or not allowed(newloc, url["url"]) then
+      tries = 0
+      return wget.actions.EXIT
+    end
+  end
+  
+  if status_code >= 200 and status_code <= 399 then
+    downloaded[url["url"]] = true
+    downloaded[string.gsub(url["url"], "https?://", "http://")] = true
+  end
+
+  if abortgrab == true then
+    io.stdout:write("ABORTING...\n")
+    io.stdout:flush()
+    return wget.actions.ABORT
+  end
+  
+  if status_code >= 500
+      or (
+        status_code >= 400
+        and status_code ~= 404
+        and status_code ~= 406
+        and status_code ~= 451
+      )
+      or status_code  == 0 then
+    io.stdout:write("Server returned "..http_stat.statcode.." ("..err.."). Sleeping.\n")
+    io.stdout:flush()
+    local maxtries = 10
+    if not allowed(url["url"], nil)
+      or (status_code == 400 and string.match(url["url"], "^https?://[^/]*qpic%.cn/")) then
+      maxtries = 3
+    end
+    if tries >= maxtries then
+      io.stdout:write("\nI give up...\n")
+      io.stdout:flush()
+      tries = 0
+      if maxtries == 3 then
+        return wget.actions.EXIT
+      else
+        return wget.actions.ABORT
+      end
+    else
+      os.execute("sleep " .. math.floor(math.pow(2, tries)))
+      tries = tries + 1
+      return wget.actions.CONTINUE
+    end
+  end
+
+  tries = 0
+
+  local sleep_time = 0
+
+  if sleep_time > 0.001 then
+    os.execute("sleep " .. sleep_time)
+  end
+
+  return wget.actions.NOTHING
+end
+
+wget.callbacks.before_exit = function(exit_status, exit_status_string)
+  if abortgrab == true then
+    return wget.exits.IO_FAIL
+  end
+  return exit_status
+end
+
